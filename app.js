1ca6ba1bf8085649867650c5cd0753d6d560cca7
github4tap <sendamailtome001@gmail.com>
2020-05-19 22:32:43 +0530

    adding few more comments to app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 * This is an multiline
 * Javascript comment
 */
-let today = new Date();
-let formatDate = today.toDateString();
+let today = new Date(); // we are creating a varibale to hold current Date 
+let formatDate = today.toDateString(); // We are formating the dateObject in this process the Time zone and other optional ppts will be removed
 
 let selectElement = document.getElementById('date');
 //console.log(selectElement.innerHTML);
